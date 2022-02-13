c54402f6c45b67a22fd52062542a26a96eddfae4
mehmetilya <mehmetilya068@gmail.com>
2022-02-13 17:13:47 +0300

    Initial commit

---
+++ System_Memory/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace System_Memory
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 256);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ System_Memory/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Deployment.Application;
+using System.Collections;
+using System.Net;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Reflection;
+using Microsoft.Win32;
+using System.Management;
+
+namespace System_Memory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CrPU();
+        }
+        private void CrPU()
+        {
+
+            var cpu = // Cpu
+    new ManagementObjectSearcher("select * from Win32_Processor")
+    .Get()
+    .Cast<ManagementObject>()
+    .First();
+
+            var isletim = // İsletim Sistemi
+            new ManagementObjectSearcher("select * from Win32_OperatingSystem")
+        .Get()
+        .Cast<ManagementObject>()
+        .First();
+
+            var video = // Ekran Kartı
+    new ManagementObjectSearcher("select * from Win32_VideoController")
+    .Get()
+    .Cast<ManagementObject>()
+    .First();
+
+            var ram = // Ram bellek
+    new ManagementObjectSearcher("select * from Win32_PhysicalMemory")
+    .Get()
+    .Cast<ManagementObject>()
+    .First();
+
+            string sistem = (string)cpu["name"];
+
+            UInt64 Capacity = 0;
+            Capacity += Convert.ToUInt64(ram["Capacity"].ToString());
+            Capacity = Capacity / 1048576;
+            Capacity = Capacity / 1024;
+            string rambellek = Capacity.ToString();
+            
+            string ekran = (string)video["name"];
+            
+            string isletimsistemi = (string)isletim["Caption"];
+
+            string sonuc = sistem + "\n" + rambellek + "\n" + isletimsistemi + "\n" + ekran;
+            label4.Text = sonuc;
+        }
+    }
+}
